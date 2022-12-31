115907ffe55dcc4c1da627595691838e701d45f0
Resji <119996970+FutileWeeb@users.noreply.github.com>
2022-12-30 18:36:07 -0800

    commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Collections.Generic; 
 
 Random dice = new Random();
-int  result = dice.Next(0,6);
+int diceResult = dice.Next(0,6);
 
-Console.WriteLine(result);
+Console.WriteLine(diceResult);
 
 
 
